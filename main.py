3fcc9302fbd2f0414bcfd7663679c9db74483342
anbarsanti <anbarsanti@nabitu.id>
2025-01-03 23:20:40 +0800

    build main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import time
 
 ## ================================== INITIALIZATION OF ROBOT COMMUNICATION STUFF ==================================
 # ROBOT_HOST = "10.149.230.168" # in robotics lab
-ROBOT_HOST = "192.168.18.12" # virtual machine in from linux host
+ROBOT_HOST = "192.168.18.14" # virtual machine in from linux host
 ROBOT_PORT = 30004
 FREQUENCY = 500 # send data in 500 Hz instead of default 125Hz
 config_filename = "control_loop_configuration.xml"
@@ -30,6 +30,17 @@ conf = rtde_config.ConfigFile(config_filename)
 state_names, state_types = conf.get_recipe("state")
 setp_names, setp_types = conf.get_recipe("setp")
 watchdog_names, watchdog_types = conf.get_recipe("watchdog")
+
+# Establish Robot Connection
+con = rtde.RTDE(ROBOT_HOST, ROBOT_PORT)
+connection_state = con.connect()
+con.get_controller_version() # Get controller version
+
+# Received the data from the robot
+con.send_output_setup(state_names, state_types, FREQUENCY)
+# Configure an input package that the external application will send to the robot controller
+setp = con.send_input_setup(setp_names, setp_types)
+watchdog = con.send_input_setup(watchdog_names, watchdog_types)
 
 setp.input_double_register_0 = 0
 setp.input_double_register_1 = 0
@@ -43,20 +54,8 @@ setp.input_bit_registers0_to_31 = 0
 # The function "rtde_set_watchdog" in the "rtde_control_loop.urp" creates a 1 Hz watchdog
 watchdog.input_double_register_0 = 0
 watchdog.input_int_register_0 = 0
-print("Initialization of Robot Communication Stuff, Alhamdulillah done")
-
-# ===========================  ESTABLISH ROBOT CONNECTION ===========================
-con = rtde.RTDE(ROBOT_HOST, ROBOT_PORT)
-connection_state = con.connect()
 print("Successfully connected to the robot:", connection_state)
-con.get_controller_version() # Get controller version
-
-# Received the data from the robot
-con.send_output_setup(state_names, state_types, FREQUENCY)
-# Configure an input package that the external application will send to the robot controller
-setp = con.send_input_setup(setp_names, setp_types)
-watchdog = con.send_input_setup(watchdog_names, watchdog_types)
-print("Setup recipes done")
+print("Initialization of Robot Communication Stuff, Alhamdulillah done")
 
 # ===================================== START ROBOT DATA SYNCHRONIZATION =====================================
 if not con.send_start():
@@ -73,7 +72,7 @@ model = YOLO("model/yolo11-hbb-toy-12-01.pt") # toys for HBB object tracking
 # model = YOLO("model/yolo11-obb-11-16-watercan.pt") # watercan for OBB object tracking
 # model = YOLO("model/yolo11n.pt") # object tracking with HBB
 
-actual_q =
+actual_q = []
 
 if OBB==True: # Initialization for OBB case
 	desired_box = [0, 0, 0, 0, 0, 0, 0, 0, 0]
